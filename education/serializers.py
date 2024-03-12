c7b58412159a9f5ecbe827e92df685a29f18bcd6
Tor4ubas <alex1029@yandex.ru>
2024-03-12 20:57:35 +0300

    добавлен SerializerMethodField()

--- education/serializers.py
+++ education/serializers.py
@@ -4,27 +4,18 @@ from education.models import Course, Lesson
 
 
 class LessonCourseSerializer(serializers.ModelSerializer):
-    """ Cериализатор для модели Course, который будет включать данные об уроках """
+    """ Cериализатор для модели Lesson """
 
     class Meta:
         model = Lesson
         fields = ('pk', 'title_lesson',)
 
 
-class CourseCreateSerializer(serializers.ModelSerializer):
-    """ Сериализатор для создания Course"""
-
-    class Meta:
-        model = Course
-        fields = ('title_course', 'description_course',)
-
-
 class CourseSerializer(serializers.ModelSerializer):
     """ Сериализатор модели Course"""
 
-    lesson_count = serializers.IntegerField(source='lesson_set.all.count', read_only=True)  # поле вывода количества уроков
+    lesson_count = serializers.SerializerMethodField()  # поле вывода количества уроков
     lessons = LessonCourseSerializer(source='lesson_set', read_only=True, many=True)  # поле вывода уроков
-    course_subscription = serializers.SerializerMethodField()  # поле подписки на курс
 
     def get_lesson_count(self, instance):
         """ Метод вывода количества уроков """
