5f05dc1e57003a87a5af57ba28b0e0c62e41e804
Tor4ubas <alex1029@yandex.ru>
2024-03-31 21:45:02 +0300

    Права доступа в DRF 25.1

--- education/permissions.py
+++ education/permissions.py
@@ -6,11 +6,6 @@ class IsManager(BasePermission):
         return request.user.groups.filter(name='Модераторы').exists()
 
 
-class IsAutorOrManager(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        return obj.author == request.user or request.user.groups.filter(name='Модераторы').exists()
-
-
 class IsAutor(BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.author == request.user
