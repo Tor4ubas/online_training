0dcc081725ce7a8a6815b44f66e2ce5dfa943272
Tor4ubas <alex1029@yandex.ru>
2024-03-31 16:22:52 +0300

    Права доступа в DRF 25.1

--- education/permissions.py
+++ education/permissions.py
@@ -2,12 +2,15 @@ from rest_framework.permissions import BasePermission
 
 
 class IsManager(BasePermission):
-
     def has_permission(self, request, view):
-        return not request.user.is_staff
+        return request.user.groups.filter(name='Модераторы').exists()
+
+
+class IsAutorOrManager(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return obj.author == request.user or request.user.groups.filter(name='Модераторы').exists()
 
 
 class IsAutor(BasePermission):
-
     def has_object_permission(self, request, view, obj):
-        return obj.author == request.user or request.user.is_staff
+        return obj.author == request.user
--- users/management/commands/csu.py
+++ users/management/commands/csu.py
@@ -1,19 +1,22 @@
-from django.core.management import BaseCommand
-
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import Group
 from users.models import User
 
 
 class Command(BaseCommand):
+    help = 'Создает фикстуру для созданных групп'
+    def handle(self, *args, **options):
+        # Создание группы модераторов
+        moderator_group, created = Group.objects.get_or_create(name='Модераторы')
 
-    def handle(self, *args, **kwargs):
+        # Создание пользователя и добавление в группы
         user = User.objects.create(
-            email='salex1029@yandex.ru',
+            email='alex1029@yandex.ru',
             first_name='Admin',
             last_name='Adminov',
-            is_superuser=True,
             is_staff=True,
             is_active=True
         )
-
         user.set_password('123qwe456rty')
         user.save()
+        user.groups.add(moderator_group)
